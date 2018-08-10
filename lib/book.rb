9723bcd69d01d26107b1d1a347b49fb914506849
rghanimah <rabiaghanimah@gmail.com>
2018-08-10 15:41:25 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
-# book.rb
-# Add you Book class here
+class Book
+  def initialize (title)
+    @title = title
+  end
+ def title
+  @title 
+  end
+ 
+ def author=(author)
+   @author=author  
+ end
+def author 
+  @author 
+end
+
+def page_count=(page_count)
+  @page_count = page_count
+end
+def page_count
+  @page_count
+end
+
+end
