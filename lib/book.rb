2be7e75309228b99111e66a9b87cf9821131726b
rghanimah <rabiaghanimah@gmail.com>
2018-08-10 15:53:57 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
-# book.rb
-# Add you Book class here
+class Book
+  
+  attr_reader :title 
+  attr_accessor :author, :genre, :page_count 
+  
+  
+  def initialize (title)
+    @title = title
+  end
+# def title
+#   @title 
+#   end
+ 
+# def author=(author)
+#   @author=author  
+# end
+# def author 
+#   @author 
+# end
+
+# def page_count=(page_count)
+#   @page_count = page_count
+# end
+# def page_count
+#   @page_count
+# end
+
+# def genre=(genre)
+#   @genre = genre
+# end 
+# def genre
+#   @genre 
+# end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end 
+end
+# wonder = Book.new("Wonder")
+# wonder.author = "RJ Palacio"
+# wonder.love
